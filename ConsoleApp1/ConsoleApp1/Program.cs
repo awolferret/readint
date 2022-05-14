b329680718baf642b0a8b4e08ebfed476aa9ead9
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-14 21:17:17 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isConverted = false;
+
+            while (isConverted == false)
+            {
+                ConvertNumber(out isConverted);
+            }
+        }
+
+        static int ConvertNumber(out bool isConverted)
+        {
+            int number = 0;
+            Console.WriteLine("Введите число");
+            string input = Console.ReadLine();
+            isConverted = int.TryParse(input, out number);
+
+            if (isConverted == true)
+            {
+                Console.WriteLine($"Ваше число: {number}");
+            }
+            else 
+            {
+                Console.WriteLine("Не удалось сконвертировать");
+            }
+            return number;
+        }
+    }
+}
